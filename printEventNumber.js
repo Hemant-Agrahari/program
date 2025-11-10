5ef699d2dc55a88c79b83bc3938f43ad79cd7655
shawn <shawn@aistechnolabs.org>
2025-11-10 12:28:59 +0530

    Print even odd number

--- printEventNumber.js
+++ printEventNumber.js
@@ -7,6 +7,24 @@ function printEvenNumber(num) {
 }
 console.log(printEvenNumber(20));
 
+function printEventNumberFromAnArray(arr) {
+  let evenNumber = [];
+  let oddNumber=[];
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i], '-', i);
+    if (arr[i] % 2 === 0) {
+      evenNumber.push(arr[i]);
+    }else{
+      oddNumber.push(arr[i])
+    }
+  }
+  return {
+    event:evenNumber,
+    odd:oddNumber
+  };
+}
+console.log(printEventNumberFromAnArray([1, 2, 3, 4, 3]));
+
 function printOddNumber(num) {
   for (let i = 1; i <= num; i++) {
     if (i % 2 !== 0) {
