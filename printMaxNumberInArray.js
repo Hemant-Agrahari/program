822d7dfe16a0af2415209ef3ceea1153c00b8618
shawn <shawn@aistechnolabs.org>
2025-11-19 12:36:09 +0530

    Fix loop index in printMaxNumberInArray and add removeDuplicatesWithoutUsingSetInArr function

--- printMaxNumberInArray.js
+++ printMaxNumberInArray.js
@@ -1,7 +1,7 @@
 function printMaxNumberInArray(arr) {
   if (arr.length === 0) return null;
   let maxValue = arr[0];
-  for (let i = 1; i <= arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     if (arr[i] > maxValue) {
       maxValue = arr[i];
     }
@@ -11,3 +11,20 @@ function printMaxNumberInArray(arr) {
 
 console.log(printMaxNumberInArray([1, 2, 3, 4, 5, 6]));
 
+function printMinMax(arr) {
+  let max = arr[0];
+  let min = arr[0];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+
+  return { min, max };
+}
+
+console.log(printMinMax([1, 12, 3, 4, 5]));
